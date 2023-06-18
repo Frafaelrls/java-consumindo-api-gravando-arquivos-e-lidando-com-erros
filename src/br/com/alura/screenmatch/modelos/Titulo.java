632383594f32cefd91b003ed4d5225d21a4d53a9
Frafaelrls <frafaelrls@gmail.com>
2023-06-18 12:03:55 -0300

    refatorando metodo toString

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -87,8 +87,8 @@ public class Titulo implements Comparable<Titulo> {
 
     @Override
     public String toString() {
-        return "nome='" + nome + '\'' +
-                ", anoDeLancamento=" + anoDeLancamento +
-                ", Duração em minutos: " + duracaoEmMinutos;
+        return "(nome = " + nome +
+                ", anoDeLancamento = " + anoDeLancamento +
+                ", Duração em minutos = " + duracaoEmMinutos + ")";
     }
 }
